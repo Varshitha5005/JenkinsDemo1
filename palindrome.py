ae1bb88c65cfb00f0d2086046d0cdee8c46d953e
Varshitha5005 <245122748010@mvsrec.edu.in>
2025-03-18 13:06:26 +0530

    C1

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+s="Hello"
+p="abba"
+if s==s[::-1]:
+    print("s is palindrome")
+else:
+    print("s is not palindrome")
+if p==p[::-1]:
+    print("p is palindrome")
+else:
+    print("p is not palindrome")
