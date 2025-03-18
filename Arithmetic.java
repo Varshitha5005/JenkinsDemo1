c85c2d11987fcb4f83dda7983d20a6b7ea88fc7d
Varshitha5005 <245122748010@mvsrec.edu.in>
2025-03-18 12:37:42 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Arithmetic
+{
+	public static void main(String[] args)
+	{
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter a");
+		int a=ob.nextInt();
+                System.out.println("Enter a");
+		int b=ob.nextInt();
+		System.out.println("Addition:"+(a+b));
+		System.out.println("Difference:"+(a-b));
+		System.out.println("Multiplication:"+(a*b));
+		System.out.println("Divsion:"+(a/b));
+	}
+}
