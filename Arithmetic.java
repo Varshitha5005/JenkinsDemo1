c2c9831ef1327c5f49ecb168e542f0a41420a332
Varshitha5005 <245122748010@mvsrec.edu.in>
2025-03-18 12:41:34 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -4,8 +4,8 @@ class Arithmetic
 	public static void main(String[] args)
 	{
 		Scanner ob=new Scanner(System.in);
-		int a=ob.nextInt();
-		int b=ob.nextInt();
+		int a=4;
+		int b=2;
 		System.out.println("Addition:"+(a+b));
 		System.out.println("Difference:"+(a-b));
 		System.out.println("Multiplication:"+(a*b));
